e99ccfffc4e064b10261acecdfb83b7e7443047e
kakau01 <leite5387@hotmail.com>
2021-02-02 11:48:19 -0300

    estruturando primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'minha-aplicacao';
+
+  title = 'Seja bem-vindo!'
+
+  //primeira funcao que é chamada quando a classe é instanciada
+  constructor() {
+    
+  }
+
+ 
 }
+
